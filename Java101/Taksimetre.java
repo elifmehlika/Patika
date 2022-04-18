a09b45a8b68281b3228c5ef6c491f175cf7797fa
Elif Mehlika Metin <metinelifmehlika@gmail.com>
2022-04-18 19:53:29 +0300

    Taksimetre eklendi

---
+++ Java101/Taksimetre.java
@@ -0,0 +1,19 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+   public static void main(String[] args) {
+
+       float startPrice = 10.0f, perKm = 2.20f, km, total;
+       Scanner scan = new Scanner(System.in);
+
+       System.out.print("Mesafeyi KM cinsinden giriniz: ");
+       km = scan.nextFloat();
+
+       total = startPrice + (km * perKm);
+       total = total < 20 ? 20 : total;
+
+       System.out.println("Tutar: " + total);
+   } 
+}
