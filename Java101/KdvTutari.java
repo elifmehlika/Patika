900094b212b96afae1015705bc14ddfb31b5ff6a
Elif Mehlika Metin <metinelifmehlika@gmail.com>
2022-04-18 19:05:16 +0300

    KdvTutari eklendi

---
+++ Java101/KdvTutari.java
@@ -0,0 +1,21 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class KdvTutari {
+    public static void main(String[] args) {
+    float kdvTutari, tutar, kdvliTutar, kdvOran;
+
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Tutarı giriniz: ");
+    tutar = scan.nextFloat();
+
+    kdvOran = tutar > 1000 ? 8.0f : 18.0f;
+
+    kdvTutari =  tutar * kdvOran / 100;
+    kdvliTutar = tutar + kdvTutari;
+
+    System.out.println("Kdv Tutarı: " + kdvTutari);
+    System.out.println("Kdv\'li Tutar: " + kdvliTutar);
+    }
+}
