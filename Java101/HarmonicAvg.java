0fe7d27f4f3e14e09d466bb3fe64f828c9f2dd86
Elif Mehlika Metin <metinelifmehlika@gmail.com>
2022-07-17 12:11:05 +0300

    HarmonicAvg eklendi

---
+++ Java101/HarmonicAvg.java
@@ -0,0 +1,19 @@
+package Java101;
+
+public class HarmonicAvg {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        double sum = 0.0, avg;
+
+        for(int i : arr){
+            sum += (1.0/i);
+            System.out.println(sum);
+        }
+
+        avg = arr.length / sum;
+
+        System.out.println("Harmonik Ortalama: " + avg);
+
+    }
+}
